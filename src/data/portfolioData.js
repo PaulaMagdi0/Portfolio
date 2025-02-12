de7f731ab1dc9ffdfb78c4ab9229754c7ae2d15b
Paula Magdy <paulamagdy665@gmail.com>
2025-02-12 21:26:18 +0200

    Restructure The Project

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,74 +1,79 @@
 export const portfolioData = {
-  hero: {
-    name: "Paula Magdy",
-    subtitle: "A programmer constantly seeking new learning opportunities.",
+  "hero": {
+    "name": "Paula Magdy",
+    "subtitle": "A PROGRAMMER WHO IS CONSTANTLY SEEKING NEW LEARNING OPPORTUNITIES.",
   },
-  about: {
-    description:
-      "I'm a full-stack developer specializing in the MERN stack, cloud technologies, and DevOps. I build scalable, high-performance web applications with a focus on responsive UI, efficient APIs, and optimized databases. Passionate about agile development and continuous learning, I thrive in collaborative environments to deliver impactful solutions.",
-    resumeLink:
-      "https://drive.google.com/file/d/1BjQiLfoLYZq17DE5DeKOZ0fSsIZiauxt/view",
+  "about": {
+    "description": "I’m a full-stack developer specializing in the MERN stack, cloud technologies, and DevOps. I build scalable, high-performance web applications with a focus on responsive UI, efficient APIs, and optimized databases. Passionate about agile development and continuous learning, I thrive in collaborative environments to deliver impactful solutions.",
+    "resumeLink": "https://drive.google.com/file/d/1BjQiLfoLYZq17DE5DeKOZ0fSsIZiauxt/view"
   },
-  projects: [
+  "projects": [
     {
-      title: "BookHub",
-      description:
-        "A full-stack book management web application built with the MERN stack (MongoDB, Express.js, React.js, and Node.js). It offers secure user authentication, efficient book catalog management, advanced search functionality, and a fully responsive UI for a seamless reading experience.",
-      liveLink: "https://bookhub-psi.vercel.app/",
-      sourceCode: "https://github.com/PaulaMagdi0/MERN-Stack-Project-ITI",
-      image: "/src/assets/bookhub.png",
+      "title": "BookHub",
+      "description": "•A full-stack book management web application built using the MERN stack (MongoDB, Express.js, React.js, and Node.js).It offers secure user authentication, efficient book catalog management, advanced search functionality, and a fully responsive UI, ensuring a seamless and intuitive reading experience.",
+      "liveLink": "https://bookhub-psi.vercel.app/",
+      "sourceCode": "https://github.com/PaulaMagdi0/MERN-Stack-Project-ITI",
+      "image": "/src/assets/bookhub.png"
     },
     {
-      title: "Trendify",
-      description:
-        "A dynamic and responsive e-commerce web application built with Vanilla JavaScript, HTML, and CSS. Features include product listings, a shopping cart, and a streamlined checkout process, delivering a seamless shopping experience.",
-      liveLink: "https://e-commerce-vanilla.vercel.app/",
-      sourceCode: "https://github.com/PaulaMagdi0/e-commerce-vanilla",
-      image: "/src/assets/trendify.png",
+      "title": "Trendify",
+      "description": "A dynamic and responsive e-commerce web application built with Vanilla JavaScript, HTML, and CSS.Features include product listings, a shopping cart, and a streamlined checkout process, delivering a seamless user shopping experience.",
+      "liveLink": "https://e-commerce-vanilla.vercel.app/",
+      "sourceCode": "https://github.com/PaulaMagdi0/e-commerce-vanilla",
+      "image": "/src/assets/trendify.png"
     },
     {
-      title: "Gericht Restaurant",
-      description:
-        "A modern and responsive restaurant website designed to showcase the menu and allow customers to book tables online. Built with HTML, CSS, JavaScript, and React.",
-      liveLink: "https://dapper-malasada-ccc5e9.netlify.app/",
-      sourceCode: "https://github.com/PaulaMagdi0/Gericht-Restaurant",
-      image: "/src/assets/Gericht.png",
+      "title": "Gericht Restaurant",
+      "description": "Developed and launched a responsive website to offer the easiest way to know what in the menu of the restaurant and booking tables for customers. By using HTML, CSS, JavaScript and React.",
+      "liveLink": "https://dapper-malasada-ccc5e9.netlify.app/",
+      "sourceCode": "https://github.com/PaulaMagdi0/Gericht-Restaurant",
+      "image": "/src/assets/Gericht.png"
     },
     {
-      title: "Shopping",
-      description:
-        "An e-commerce website for a clothing store, providing a catalog of products available for purchase. Built using HTML, CSS, JavaScript, and React to deliver a smooth and responsive user experience.",
-      liveLink: "https://peaceful-praline-2d4d8e.netlify.app/",
-      sourceCode: "https://github.com/PaulaMagdi0/E_commerce",
-      image: "/src/assets/Shopping.png",
+      "title": "Shopping",
+      "description": "This project deals with developing an e-commerce website for Clothes Store. It provides the user with a catalog of different product available for purchase in the store. Developed and launched a responsive website using HTML, CSS, JavaScript and React.",
+      "liveLink": "https://peaceful-praline-2d4d8e.netlify.app/",
+      "sourceCode": "https://github.com/PaulaMagdi0/E_commerce",
+      "image": "/src/assets/Shopping.png"
     },
     {
-      title: "Rock Paper Scissors",
-      description:
-        "A fun and interactive web-based game where users can play Rock, Paper, Scissors against the computer or challenge a friend. Built with HTML, CSS, and JavaScript.",
-      liveLink:
-        "https://rock-paper-scissors-71wy7al53-paula-magdys-projects.vercel.app/",
-      sourceCode: "https://github.com/PaulaMagdi0/Rock-Paper-Scissors",
-      image: "/src/assets/rock-paper-scissors.png",
+      "title": "Rock Paper Scissors",
+      "description": "A fun and interactive web-based game of Rock, Paper, Scissors, where users can play against the computer or challenge a friend. Built using [HTML, CSS, and JavaScript] to create a responsive and interactive user interface.",
+      "liveLink": "https://rock-paper-scissors-71wy7al53-paula-magdys-projects.vercel.app/",
+      "sourceCode": "https://github.com/PaulaMagdi0/Rock-Paper-Scissors",
+      "image": "/src/assets/rock-paper-scissors.png"
     },
+    // {
+    //   "title": "Calculator",
+    //   "description": "A comprehensive online scientific calculator that performs mathematical operations. Built using [HTML, CSS, and JavaScript] to create a responsive and interactive user interface.",
+    //   "liveLink": "https://calculator-sigma-sandy.vercel.app/",
+    //   "sourceCode": "https://github.com/PaulaMagdi0/Calculator",
+    //   "image": "assets/calculator-1.png"
+    // },
+    // {
+    //   "title": "Age Calculator",
+    //   "description": "A simple yet effective web application that calculates a user's age in years, months, and days based on their birthdate. Built using [HTML, CSS, and JavaScript] to create a responsive and interactive user interface.",
+    //   "liveLink": "https://age-calculator-iota-hazel.vercel.app/",
+    //   "sourceCode": "https://github.com/PaulaMagdi0/Age-Calculator",
+    //   "image": "assets/age-calculator.png"
+    // },
     {
-      title: "RoboFriends",
-      description:
-        "My first React application that allows users to interact with virtual robots and implements Redux for state management. Developed a responsive interface using HTML, CSS, JavaScript, React, and Redux.",
-      liveLink: "https://bucolic-medovik-9c990e.netlify.app/",
-      sourceCode: "https://github.com/PaulaMagdi0/RoboFriends",
-      image: "/src/assets/RoboFriends.png",
+      "title": "RoboFriends",
+      "description": "It was my first react application to play with robots and add Redux to the app. Developed and launched a responsive website using HTML, CSS, JavaScript, React and Redux.",
+      "liveLink": "https://bucolic-medovik-9c990e.netlify.app/",
+      "sourceCode": "https://github.com/PaulaMagdi0/RoboFriends",
+      "image": "/src/assets/RoboFriends.png"
     },
   ],
-  contact: {
-    phone: "+02 01277767028",
-    email: "paulamagdy665@gmail.com",
+  "contact": {
+    "phone": "+02 01277767028",
+    "email": "paulamagdy665@gmail.com",
   },
-  social: {
-    facebook: "https://www.facebook.com/elneamer.pop",
-    linkedin: "https://www.linkedin.com/in/paula-magdy/",
-    github: "https://github.com/PaulaMagdi0",
-    leetcode: "https://leetcode.com/u/4pAckTIlBP/",
-    hackerrank: "https://www.hackerrank.com/profile/paulamagdy665",
-  },
-};
+  "social": {
+    "facebook": "https://www.facebook.com/elneamer.pop",
+    "linkedin": "https://www.linkedin.com/in/paula-magdy/",
+    "github": "https://github.com/PaulaMagdi0",
+    "leetcode": "https://leetcode.com/u/4pAckTIlBP/",
+    "hackerrank": "https://www.hackerrank.com/profile/paulamagdy665"
+  }
+}
