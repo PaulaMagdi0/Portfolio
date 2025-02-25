eeb7832cec43a7d6add863b9e26fe58d8afec654
Paula Magdy <81883302+PaulaMagdi0@users.noreply.github.com>
2025-02-25 19:39:35 +0200

    Update portfolioData

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -4,34 +4,34 @@ export const portfolioData = {
     "subtitle": "A PROGRAMMER WHO IS CONSTANTLY SEEKING NEW LEARNING OPPORTUNITIES.",
   },
   "about": {
-    "description": "I’m a full-stack developer specializing in the MERN stack, cloud technologies, and DevOps. I build scalable, high-performance web applications with a focus on responsive UI, efficient APIs, and optimized databases. Passionate about agile development and continuous learning, I thrive in collaborative environments to deliver impactful solutions.",
-    "resumeLink": "https://drive.google.com/file/d/1BjQiLfoLYZq17DE5DeKOZ0fSsIZiauxt/view"
+    "description": "I’m a full-stack developer specializing in the MERN stack, cloud technologies, and DevOps. I build scalable, high-performance web applications focusing on responsive UI, efficient APIs, and optimized databases. Passionate about agile development and continuous learning, I thrive in collaborative environments to deliver impactful solutions.",
+    "resumeLink": "https://drive.google.com/file/d/1vv6g8KCvNfU8AxnI_97787S091-V9G_P/view"
   },
   "projects": [
     {
       "title": "BookHub",
-      "description": "A full-stack book management web application built using the MERN stack (MongoDB, Express.js, React.js, and Node.js).It offers secure user authentication, efficient book catalog management, advanced search functionality, and a fully responsive UI, ensuring a seamless and intuitive reading experience.",
+      "description": "A full-stack book management web application built using the MERN stack (MongoDB, Express.js, React.js, and Node.js). It offers secure user authentication, efficient book catalog management, advanced search functionality, and a fully responsive UI, ensuring a seamless and intuitive reading experience.",
       "liveLink": "https://bookhub-psi.vercel.app/",
       "sourceCode": "https://github.com/PaulaMagdi0/MERN-Stack-Project-ITI",
       "image": "https://res.cloudinary.com/dprqu9dci/image/upload/v1739388531/bookhub_cmuymd.png"
     },
     {
       "title": "Trendify",
-      "description": "A dynamic and responsive e-commerce web application built with Vanilla JavaScript, HTML, and CSS.Features include product listings, a shopping cart, and a streamlined checkout process, delivering a seamless user shopping experience.",
+      "description": "A dynamic and responsive e-commerce web application built with Vanilla JavaScript, HTML, and CSS. Features include product listings, a shopping cart, and a streamlined checkout process, delivering a seamless user shopping experience.",
       "liveLink": "https://e-commerce-vanilla.vercel.app/",
       "sourceCode": "https://github.com/PaulaMagdi0/e-commerce-vanilla",
       "image": "https://res.cloudinary.com/dprqu9dci/image/upload/v1739388536/trendify_nvgkm7.png"
     },
     {
       "title": "Gericht Restaurant",
-      "description": "Developed and launched a responsive website to offer the easiest way to know what in the menu of the restaurant and booking tables for customers. By using HTML, CSS, JavaScript and React.",
+      "description": "Developed and launched a responsive website to offer the easiest way to know what is in the restaurant's menu and booking tables for customers. By using HTML, CSS, JavaScript, and React.",
       "liveLink": "https://dapper-malasada-ccc5e9.netlify.app/",
       "sourceCode": "https://github.com/PaulaMagdi0/Gericht-Restaurant",
       "image": "https://res.cloudinary.com/dprqu9dci/image/upload/v1739388535/Gericht_d7tjls.png"
     },
     {
       "title": "Shopping",
-      "description": "This project deals with developing an e-commerce website for Clothes Store. It provides the user with a catalog of different product available for purchase in the store. Developed and launched a responsive website using HTML, CSS, JavaScript and React.",
+      "description": "This project deals with developing an e-commerce website for Clothes Store. It provides the user with a catalog of different product available for purchase in the store. Developed and launched a responsive website using HTML, CSS, JavaScript, and React.",
       "liveLink": "https://peaceful-praline-2d4d8e.netlify.app/",
       "sourceCode": "https://github.com/PaulaMagdi0/E_commerce",
       "image": "https://res.cloudinary.com/dprqu9dci/image/upload/v1739388536/Shopping_exlaup.png"
@@ -59,7 +59,7 @@ export const portfolioData = {
     // },
     {
       "title": "RoboFriends",
-      "description": "It was my first react application to play with robots and add Redux to the app. Developed and launched a responsive website using HTML, CSS, JavaScript, React and Redux.",
+      "description": "It was my first react application to play with robots and add Redux to the app. Developed and launched a responsive website using HTML, CSS, JavaScript, React, and Redux.",
       "liveLink": "https://bucolic-medovik-9c990e.netlify.app/",
       "sourceCode": "https://github.com/PaulaMagdi0/RoboFriends",
       "image": "https://res.cloudinary.com/dprqu9dci/image/upload/v1739388535/RoboFriends_tao5x8.png"
